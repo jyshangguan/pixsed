7ecdaffca8ad9ab35b0f31c7033205de716a3b87
luoyilin <2100011062@stu.pku.edu.cn>
2023-07-03 17:08:44 +0800

    Add error estimation functions.

--- pixsed/utils.py
+++ pixsed/utils.py
@@ -1,16 +1,17 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import astropy.units as u
+import matplotlib.pyplot as plt
+import numpy as np
 from astropy.coordinates import SkyCoord
+from astropy.stats import sigma_clipped_stats
 from astropy.visualization import AsinhStretch, SqrtStretch, LogStretch
-from astropy.visualization import ImageNormalize, PercentileInterval
+from astropy.visualization import PercentileInterval
 from astropy.visualization.mpl_normalize import ImageNormalize
 
 stretchDict = {'asinh': AsinhStretch(), 'sqrt': SqrtStretch(), 'log': LogStretch()}
 
 
-def plot_image(data, ax=None, percentile=99.5, vmin=None, vmax=None, stretch=None, 
-               origin='lower', cmap='gray_r', **kwargs):
+def plot_image(data, ax=None, percentile=99.5, vmin=None, vmax=None, stretch=None,
+               origin='lower', cmap='gray', **kwargs):
     """
     Plot the image.
 
@@ -49,7 +50,7 @@ def plot_image(data, ax=None, percentile=99.5, vmin=None, vmax=None, stretch=Non
     norm = ImageNormalize(interval=PercentileInterval(percentile), vmin=vmin, vmax=vmax, stretch=stretch)
     if 'norm' not in kwargs:
         kwargs['norm'] = norm
-        
+
     ax.imshow(data, origin=origin, cmap=cmap, **kwargs)
     ax.minorticks_on()
     return ax
@@ -78,3 +79,29 @@ def read_coordinate(ra, dec):
         c = SkyCoord(ra, dec, frame='icrs', unit='deg')
     return c
 
+
+def circular_error_estimate(img, mask, radius, nexample):
+    std_list = []
+    count = 0
+    xllim = radius + 1
+    xhlim = np.shape(img)[0] - radius - 1
+    yllim = radius + 1
+    yhlim = np.shape(img)[1] - radius - 1
+    while count <= nexample:
+        x_cen = np.random.randint(xllim, xhlim)
+        y_cen = np.random.randint(yllim, yhlim)
+        if mask[y_cen, x_cen] or not isinstance(img[y_cen, x_cen], float):
+            continue
+        flag = 0
+        data_sum = 0
+        for ar in range(-radius, radius + 1):
+            for br in range(-radius, radius + 1):
+                if mask[y_cen + ar, x_cen + br] or not isinstance(img[y_cen + ar, x_cen + br], float):
+                    flag = 1
+                else:
+                    data_sum += img[y_cen + ar, x_cen + br]
+        if flag == 0:
+            count += 1
+            std_list.append(data_sum)
+    mean, mead, std = sigma_clipped_stats(np.array(std_list))
+    return std
